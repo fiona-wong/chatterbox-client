d8c6b5bb566e4f034300a4ae23ac004b3cccf2a9
Hack Reactor Student <student@hackreactor.com>
2017-07-14 20:19:44 -0700

    Working on events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,113 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
+};
+
+app.init = function() {
+  this.handleUsernameClick();
+};
+
+app.send = function(message) { 
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+app.fetch = function(link, data) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'GET',
+    data: console.log(data),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
+};
+
+
+app.clearMessages = function() { 
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: $('#chats').empty(),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Messages cleared');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to clear messages', data);
+    }
+  });
+};
+
+app.renderMessage = function(obj) {
+  //var textNode = document.createTextNode(obj.message);
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: $('#chats').append('<p>obj</p>'),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.renderRoom = function(room) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server,
+    type: 'POST',
+    data: $('#roomSelect').append('<p>room</p>'),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+  
+
+app.handleUsernameClick = function() {
+  $('.username').click(function() {
+    alert('Add friend?');
+  });   
+};
+
+
+
+
+
+
+
+
+
+
+
